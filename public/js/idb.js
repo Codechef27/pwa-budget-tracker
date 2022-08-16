43edec8190087cb8a973be3f6db4196acae4b3af
BradleyK <bradleyjkennett@gmail.com>
2022-08-16 16:28:06 -0400

    remove useFindandModify to try to get indexdb to work

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,41 +9,41 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
 
     db.createObjectStore('new_budget_data', { autoIncrement: true } ); 
-};
+}
 
 request.onsuccess = function(event) {
     db = event.target.result;
 
-    if(navigator.online) {
+    if(navigator.onLine) {
         uploadBudgetData();
     }
-};
+}
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
-};
+}
 
 
-function saveData(data)  {
+function saveData(record)  {
     const transaction = db.transaction(['new_budget_data'], 'readwrite');
 
     const dataObjectStore = transaction.objectStore('new_budget_data');
 
-    dataObjectStore.add(data);
-};
+    dataObjectStore.add(record);
+}
 
 function uploadBudgetData()  {
     const transaction = db.transaction(['new_budget_data'], 'readwrite');
 
     const dataObjectStore = transaction.objectStore('new_budget_data');
 
-    const getData = dataObjectStore.getAll();
+    const getAll = dataObjectStore.getAll();
 
-    getData.onsuccess = () => {
-        if(getData.result.length > 0){
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0){
             fetch('/api/transaction/bulk' ,{
                 method: 'POST',
-                body: JSON.stringify(getData.result),
+                body: JSON.stringify(getAll.result),
                 headers: {
                     Accept: "application/json, text/plain, */*",
                     "Content-Type": "application/json" 
