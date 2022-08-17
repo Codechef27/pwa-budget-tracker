c4b27e7bfa20333eced441697f564bc0d2e22166
BradleyK <bradleyjkennett@gmail.com>
2022-08-17 08:53:10 -0400

    fix load issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,10 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./js/idb.js",
     "./css/styles.css",
-    "./js/index.js",
     "./manifest.json",
     "./icons/icon-72x72.png",
     "./icons/icon-96x96.png",
