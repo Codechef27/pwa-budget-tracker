d822c5d0e2b5e644f2d9c4f94274237a39a78402
BradleyK <bradleyjkennett@gmail.com>
2022-08-17 09:07:13 -0400

    bug issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/",
+    "../../server.js",
     "./index.html",
     "./js/idb.js",
     "./css/styles.css",
